4cacc52092c4fa58cd13dcbd5eda79195fd787c1
toumi245 <chedlit5@gmail.com>
2023-09-19 12:51:27 +0100

    qs

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express=require('express')
-
+const cors=require('cors')
 const app=express()
 const port=process.env.port ||6001
 const dbConnection=require('./config/db')
@@ -8,6 +8,8 @@ app.use(express.json())
 app.use("/api/cars/",require("./routes/carsRoute"))
 app.use("/api/users/",require("./routes/usersRoute"))
 app.use("/api/bookings/" ,require("./routes/bookingsRoute"))
-
+app.use(cors({
+    origin:["http://localhost:3000","https://mern-rentacar-frontend.onrender.com"]
+}))
 app.get('/',(req,res)=>res.send('hello world'))
 app.listen(port,()=>console.log(`node js server started in port ${port} `))
